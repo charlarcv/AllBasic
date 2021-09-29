e0cb4db181f374b9aaab9c949fbbe6b2021c4657
charlarcv <charlarcv@gmail.com>
2021-09-29 06:42:31 +0400

    Added New test Class First API and API testing methods

---
+++ src/test/java/api_Automation/API_AutomationTestTest.java
@@ -0,0 +1,33 @@
+package api_Automation;
+
+import org.testng.annotations.Test;
+
+public class API_AutomationTestTest {
+
+  @Test
+  public void apiTest() {
+	  
+	  int r=1;
+	  int g=1;
+	  int add=r+g;
+	  System.out.println(add);
+    throw new RuntimeException("Test not implemented");
+    
+  }
+
+  @Test
+  public void api2Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void api3Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void api4Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+  
+}
