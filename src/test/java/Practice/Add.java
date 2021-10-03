858203a8ae0b513ee4c5d20fb74a9b708adc8549
charlarcv <charlarcv@gmail.com>
2021-10-03 14:40:06 +0400

    asdfg

---
+++ src/test/java/Practice/Add.java
@@ -0,0 +1,21 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the number: ");
+			int n=sc.nextInt();
+			int v=sc.nextInt();
+			System.out.println(n+v);
+			System.out.println(n/v);
+			
+
+	}
+
+}
